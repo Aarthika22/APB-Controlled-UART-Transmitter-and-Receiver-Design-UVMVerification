32eeb8b9b4e1ec096c82ed047e143f1542d7e583
Aarthika22 <aarthika905@gmail.com>
2026-06-25 22:04:57 +0530

    Update apb_agt.sv

--- apb_agt_top/apb_agt.sv
+++ apb_agt_top/apb_agt.sv
@@ -1,25 +1,25 @@
 class apb_agt extends uvm_agent;
-`uvm_component_utils(apb_agt)
+  `uvm_component_utils(apb_agt)
 
-function new(string name ="apb_agt",uvm_component parent);
-super.new(name,parent);
-endfunction
+  function new(string name ="apb_agt",uvm_component parent);
+    super.new(name,parent);
+  endfunction
 
-apb_drv apb_drv_h;
-apb_mon apb_mon_h;
-apb_seqr apb_seqr_h;
+  apb_drv apb_drv_h;
+  apb_mon apb_mon_h;
+  apb_seqr apb_seqr_h;
 
-function void build_phase(uvm_phase phase);
-super.build_phase(phase);
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
 
-apb_drv_h = apb_drv::type_id::create("apb_drv_h",this);
-apb_mon_h = apb_mon::type_id::create("apb_mon_h",this);
-apb_seqr_h = apb_seqr::type_id::create("apb_seqr_h",this);
+    apb_drv_h = apb_drv::type_id::create("apb_drv_h",this);
+    apb_mon_h = apb_mon::type_id::create("apb_mon_h",this);
+    apb_seqr_h = apb_seqr::type_id::create("apb_seqr_h",this);
 
-endfunction
+  endfunction
 
-function void connect_phase(uvm_phase phase);
-super.connect_phase(phase);
-apb_drv_h.seq_item_port.connect(apb_seqr_h.seq_item_export);
-endfunction
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    apb_drv_h.seq_item_port.connect(apb_seqr_h.seq_item_export);
+  endfunction
 endclass
